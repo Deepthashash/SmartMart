6d41b1c0db58e2af13c99eda0bd1949b369cd680
asangika <36620162+asangika@users.noreply.github.com>
2020-01-18 10:31:32 +0530

    preorder module created

---
+++ src/app/layouts/preorder/preorder.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PreorderComponent } from './preorder.component';
+
+@NgModule({
+  declarations: [PreorderComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class PreorderModule { }
